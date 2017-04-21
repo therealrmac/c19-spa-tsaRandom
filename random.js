76d13d1aef0aff3426b621a001c3c3da8694d934
Nashville Software School <nashvillesoftwareschool@Nashvilles-MacBook-Pro.local>
2017-04-21 12:47:49 -0500

    update to the javascript and css

--- random.js
+++ random.js
@@ -3,23 +3,16 @@ var leftArrow= document.getElementById('arrow-left');
 var rightArrow= document.getElementById('arrow-right');
 
 
-var test= document.getElementById('testArea');
+var test= document.getElementById('clickArea');
 
 test.addEventListener('mousedown', function(){
+
 	var x= Math.floor(Math.random()*6 +1);
 	console.log(x);
-	if(x ===1 || x===2 || x===3){
-	if(rightArrow.style.visibility ==="hidden"){
-		rightArrow.style.visibility= 'visible';
-	}else{
-		rightArrow.style.visibility= "hidden";
+	if(x <=3){
+		rightArrow.classList.toggle('hidden');
 	}
-}
-	if(x===4 || x===5 || x===6){
-	if(leftArrow.style.visibility ==="hidden"){
-		leftArrow.style.visibility= 'visible';
-	}else{
-		leftArrow.style.visibility= "hidden";
+	if(x >=4){
+		leftArrow.classList.toggle('hidden')
 	}
-}
 });
